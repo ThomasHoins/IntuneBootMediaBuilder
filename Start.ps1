f4a7630b498e2b7cc72d6864368d7ddd12b87c1f
TomTom1492 <55402211+ThomasHoins@users.noreply.github.com>
2024-11-19 21:26:56 +0100

    Update Start.ps1

--- Start.ps1
+++ Start.ps1
@@ -1,5 +1,5 @@
 #Download FIDO
-Invoke-Webrequest https://raw.githubusercontent.com/pbatard/Fido/refs/heads/master/Fido.ps1" -Outfile X:\Users\Public\Downloads\Fido.ps1
+Invoke-Webrequest "https://raw.githubusercontent.com/pbatard/Fido/refs/heads/master/Fido.ps1" -Outfile X:\Users\Public\Downloads\Fido.ps1
 #get missing drivers
 #Get-WmiObject Win32_PNPEntity | Where-Object{[string]::IsNullOrEmpty($_.ClassGuid)
 
