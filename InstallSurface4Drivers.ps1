2340ace7202baa51203d56d6991fc17bf8ff251b
TomTom1492 <55402211+ThomasHoins@users.noreply.github.com>
2024-11-19 22:31:52 +0100

    Create InstallSurface4Drivers.ps1

---
+++ InstallSurface4Drivers.ps1
@@ -0,0 +1,2 @@
+Invoke-Webrequest "https://download.microsoft.com/download/f/7/0/f70b3d0a-59b1-4842-9130-0c152bb738ba/SurfaceLaptop4_Intel_Win11_22621_24.102.19170.0.msi" -Outfile "X:\Users\Public\Downloads\SurfaceLaptop4_Intel_Win11_22621_24.102.19170.0.msi"
+"X:\Users\Public\Downloads\SurfaceLaptop4_Intel_Win11_22621_24.102.19170.0.msi"
