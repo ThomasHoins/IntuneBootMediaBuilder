bc85b0a7d33a6982b42be1791b4afae1d518e539
TomTom1492 <55402211+ThomasHoins@users.noreply.github.com>
2024-11-19 21:47:31 +0100

    Update Start.ps1

--- Start.ps1
+++ Start.ps1
@@ -1,8 +1,14 @@
 #Download FIDO
-Invoke-Webrequest "https://raw.githubusercontent.com/pbatard/Fido/refs/heads/master/Fido.ps1" -Outfile X:\Users\Public\Downloads\Fido.ps1
+Invoke-Webrequest "https://raw.githubusercontent.com/pbatard/Fido/refs/heads/master/Fido.ps1" -Outfile "X:\Users\Public\Downloads\Fido.ps1"
+Invoke-Webrequest "https://raw.githubusercontent.com/ThomasHoins/IntuneInstall/refs/heads/main/InstallDrivers.ps1" -Outfile "X:\Users\Public\Downloads\InstallDrivers.ps1"
+
+
 #get missing drivers
-#Get-WmiObject Win32_PNPEntity | Where-Object{[string]::IsNullOrEmpty($_.ClassGuid)
+Get-WmiObject Win32_PNPEntity | Where-Object{[string]::IsNullOrEmpty($_.ClassGuid)
+
+#Install Drivers
+"X:\Users\Public\Downloads\InstallDrivers.ps1"
 
 
 #Start FIDO
-X:\Users\Public\Downloads\Fido.ps1
+"X:\Users\Public\Downloads\Fido.ps1"
