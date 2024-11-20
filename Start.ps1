5aed0598d07c325808b061f0b6ad48b102962de0
TomTom1492 <55402211+ThomasHoins@users.noreply.github.com>
2024-11-20 21:04:00 +0100

    Update Start.ps1

--- Start.ps1
+++ Start.ps1
@@ -1,20 +1,13 @@
 #Download FIDO
 Invoke-Webrequest "https://raw.githubusercontent.com/pbatard/Fido/refs/heads/master/Fido.ps1" -Outfile "X:\Users\Public\Downloads\Fido.ps1"
-#Download InstallDrivers
-#Invoke-Webrequest "https://raw.githubusercontent.com/ThomasHoins/IntuneInstall/refs/heads/main/InstallDrivers.ps1" -Outfile "X:\Users\Public\Downloads\InstallDrivers.ps1"
-#Download msiexec
-Invoke-Webrequest "https://raw.githubusercontent.com/ThomasHoins/IntuneInstall/refs/heads/main/msiexec.exe" -Outfile "X:\Windows\System32\msiexec.exe"
+Invoke-Webrequest "https://catalog.s.download.windowsupdate.com/c/msdownload/update/driver/drvs/2024/09/3e4f6712-9575-4960-98a5-9dfd6e19ee7f_7b36505e0cb7e03b1838f13d2b4453193599030d.cab -Outfile "X:\Users\Public\Downloads\Surface_HIDClass_3.98.10.0.cab"
+"X:\Users\Public\Downloads\Surface_HIDClass_3.98.10.0.cab"
+md X:\Users\Public\Downloads\Surface4
+expand.exe "X:\Users\Public\Downloads\Surface_HIDClass_3.98.10.0.cab" -F:* "X:\Users\Public\Downloads\Surface4"
+Get-ChildItem -Path "X:\Users\Public\Downloads\Surface4" -Filter *.inf -Recurse -ErrorAction SilentlyContinue -Force | %{ drvload $_.FullName}
 
-#Download InstallSurface4Drivers
-Invoke-Webrequest "https://raw.githubusercontent.com/ThomasHoins/IntuneInstall/refs/heads/main/InstallSurface4Drivers.ps1" -Outfile "X:\Users\Public\Downloads\InstallSurface4Drivers.ps1"
-#Install Surface Drivers
-"X:\Users\Public\Downloads\InstallSurface4Drivers.ps1"
-
-#get missing drivers
+#"get missing drivers
 Get-WmiObject Win32_PNPEntity | Where-Object{[string]::IsNullOrEmpty($_.ClassGuid)}|select Caption, CreationClassName, HardwareID |ft
-
-#Install Drivers
-#"X:\Users\Public\Downloads\InstallDrivers.ps1"
 
 
 #Start FIDO
