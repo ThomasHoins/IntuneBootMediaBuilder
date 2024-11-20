4d468a0abb192c1e7694098e60e4e64d921d11b1
TomTom1492 <55402211+ThomasHoins@users.noreply.github.com>
2024-11-20 16:56:52 +0100

    Update InstallSurface4Drivers.ps1

--- InstallSurface4Drivers.ps1
+++ InstallSurface4Drivers.ps1
@@ -1,7 +1,7 @@
 Invoke-Webrequest "https://download.microsoft.com/download/f/7/0/f70b3d0a-59b1-4842-9130-0c152bb738ba/SurfaceLaptop4_Intel_Win11_22621_24.102.19170.0.msi" -Outfile "X:\Users\Public\Downloads\SurfaceLaptop4_Intel_Win11_22621_24.102.19170.0.msi"
 Invoke-Webrequest "https://msdl.microsoft.com/download/symbols/msiexec.exe/DD894DD217000/msiexec.exe" -Outfile "X:\Users\Public\Downloads\msiexec.exe"
 
-"X:\Users\Public\Downloads\msiexec.exe" /a "X:\Users\Public\Downloads\SurfaceLaptop4_Intel_Win11_22621_24.102.19170.0.msi" targetdir="X:\Users\Public\Downloads\surface4_laptop_drivers" /qn
+"X:\Windows\System32\msiexec.exe" /i "X:\Users\Public\Downloads\SurfaceLaptop4_Intel_Win11_22621_24.102.19170.0.msi" /qn
 
 TglSerial
 IntelPreciseTouch
