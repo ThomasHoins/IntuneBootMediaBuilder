2544414353763a455cc99829b8387c36bcb7813e
TomTom1492 <55402211+ThomasHoins@users.noreply.github.com>
2024-11-26 11:27:32 +0100

    Update Start.ps1

--- Start.ps1
+++ Start.ps1
@@ -1,6 +1,6 @@
 #Download FIDO
 Invoke-Webrequest "https://raw.githubusercontent.com/pbatard/Fido/refs/heads/master/Fido.ps1" -Outfile "X:\Users\Public\Downloads\Fido.ps1"
-Invoke-Webrequest "https://catalog.s.download.windowsupdate.com/c/msdownload/update/driver/drvs/2024/09/3e4f6712-9575-4960-98a5-9dfd6e19ee7f_7b36505e0cb7e03b1838f13d2b4453193599030d.cab" -Outfile "X:\Users\Public\Downloads\Surface_HIDClass_3.98.10.0.cab"
+#Invoke-Webrequest "https://catalog.s.download.windowsupdate.com/c/msdownload/update/driver/drvs/2024/09/3e4f6712-9575-4960-98a5-9dfd6e19ee7f_7b36505e0cb7e03b1838f13d2b4453193599030d.cab" -Outfile "X:\Users\Public\Downloads\Surface_HIDClass_3.98.10.0.cab"
 
 #md X:\Users\Public\Downloads\Surface4
 #expand.exe "X:\Users\Public\Downloads\Surface_HIDClass_3.98.10.0.cab" -F:* "X:\Users\Public\Downloads\Surface4"
